6b74f6abda646d71f399982620448b58270e696b
Ire <48474962+Warszawa1@users.noreply.github.com>
2021-12-28 15:50:06 +0100

    Update and rename app.js to script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/favicon-16x16.png">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <title>Ǝncɹypʇeɹ</title>
+
+</head>
+<body>
+<div class="container">
+    <h1>Encryption / Decryption tool 🔐 </h1>
+</div>
+<div class="field">
+    <label for= "entry">Entry of your text: </label>
+</br>    
+    <textarea name="txtArea" class="txtArea" id="encryptText" rows="11" cols="45" class="txtUser" placeholder="Type something to encrypt"></textarea>
+</br>
+    <button type="button" class="button" id= "encrypt"> ENCRYPT ⏬ </button>
+</div>
+<div class="field">
+    <label for= "entry">Output of your encrypted text 🔒 </label></br>  
+    <textarea name="txtArea2" class="txtArea" id="decryptText" rows="11" cols="45" class="txtResult" placeholder="Your message after Encryption"></textarea>
+    <button type="button" class="button" id ="decrypt"> DECRYPT ⏬ </button>
+</div>
+
+<div class="field">
+<label for= "entry">Your decrypted text 🔑 </label></br>    
+    <textarea name="txtArea3" class="txtArea" id="backToNormal" rows="11" cols="45" class="txtUser" placeholder="Here you have your original text"></textarea>
+    <button type="button" class="button" id ="reset" onClick="window.location.reload();"> RESET 🔄 </button>
+</div>
+</div>
+
+     
+<script src= "script.js" ></script>
+</body>
+</html>
